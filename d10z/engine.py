2c8487cc7e2b8bc71f23d98c49d94e43c8cde92e
D10Z <jamilquecto@gmail.com>
2025-11-18 04:13:13 -0500

    Refactor nodal_energy to accept Z_N as an argument
    Updated nodal_energy function to include Z_N as a parameter.

--- d10z/engine.py
+++ d10z/engine.py
@@ -1,21 +1,9 @@
-import math
-from . import Z_N
-
-def nodal_energy(x, f=1.0, v=1.0):
-    """
-    Calcula la energía nodal según TTA–D10Z:
-
-        E_x = f * sqrt(v * ln(x / Z_n))
-
-    Donde x debe representar una magnitud medible:
-    - intensidad señal
-    - eficiencia
-    - densidad nodal
-    """
+def nodal_energy(x, f=1.0, v=1.0, Z_N=1e-51):
+    import math
     if x <= 0:
         return 0.0
-
     try:
         return f * math.sqrt(v * math.log(x / Z_N))
     except ValueError:
         return 0.0
+
